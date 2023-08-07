044e8bd43985de2dc2bc81c31f2fed892d466140
Vidit <viditgoel2k@gmail.com>
2023-08-07 22:37:52 +0530

    cards added and hover fixed.

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 let crsr = document.querySelector("#cursor");
 let blur = document.querySelector("#cursor-blur");
 document.addEventListener('mousemove', function(dets){
-    setTimeout(() => {
-        crsr.style.left = dets.x-10+"px";
-        crsr.style.top = dets.y-10+"px";
-    }, 150);
-    setTimeout(() => {
-        blur.style.left = dets.x - 225 +"px";
-        blur.style.top = dets.y - 225 +"px";
-    }, 300);
+    crsr.style.left = dets.x-17+"px";
+    crsr.style.top = dets.y-17+"px";
+    blur.style.left = dets.x - 225 +"px";
+    blur.style.top = dets.y - 225 +"px";
 });
 
-gsap.to("#header", {
+gsap.to("#nav", {
     backgroundColor : "#000000",
     height: "80px" ,
     duration: "0.5",
