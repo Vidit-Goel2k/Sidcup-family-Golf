f95c88c6cf5fc75b182a2d85ba52ed3f043380ee
Vidit <viditgoel2k@gmail.com>
2023-08-08 20:32:31 +0530

    about us and cards section scroll animation added

--- script.js
+++ script.js
@@ -48,3 +48,37 @@ gsap.to("#main",{
         scrub:2,
     }
 });
+
+gsap.from("#about-us img, #about-us-in", {
+    y:90,
+    opacity:0,
+    duration:1,
+    scrollTrigger:{
+        trigger:"#about-us",
+        scroller:"body",
+        // markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:1,
+    },
+});
+
+gsap.from(".card", {
+    scale:0.5,
+    opacity:0,
+    duration:1,
+    scrollTrigger:{
+        trigger:".card",
+        scroller:"body",
+        // markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:1,
+    },
+});
+
+// gsap.from("#quote-left", {
+//     x:-70,
+//     x:-70,
+
+// })
